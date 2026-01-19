c51776f9a1b10927e253b86542233a604e52930e
bansari006 <bansarigajera10@gmail.com>
2026-01-19 06:57:06 +0530

    Add files via upload

---
+++ LAB 1 PY 15.py
@@ -0,0 +1,5 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> f = float(input("Enter temperature in Fahrenheit: "))
+... c = (5/9) * (f - 32)
+... print("Temperature in Celsius:", c)
---
+++ LAB 1 PY 16.py
@@ -0,0 +1,8 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> p = float(input("Enter principal amount: "))
+... r = float(input("Enter rate of interest: "))
+... n = float(input("Enter time (years): "))
+... 
+... i = (p * r * n) / 100
+... print("Simple Interest:", i)
